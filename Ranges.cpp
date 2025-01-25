df3ae6c0c76ef3e54d7809944949ef920504c8c7
Dongle <96672879+hijin-dong@users.noreply.github.com>
2025-01-25 21:01:16 +0900

    Create Ranges.cpp

---
+++ Ranges.cpp
@@ -0,0 +1,44 @@
+#include "Ranges.h"
+
+Ranges::Ranges(unsigned end)
+    : Ranges(0, end)
+{}
+
+Ranges::Ranges(unsigned begin, unsigned end)
+    : _begin(begin), _end(end)
+{}
+
+Ranges::Iterator Ranges::begin() const
+{
+    return Iterator(_begin);
+}
+
+Ranges::Iterator Ranges::end() const
+{
+    return Iterator(_end);
+}
+
+Ranges::Iterator::Iterator(unsigned num)
+    : _num(num)
+{}
+
+Ranges::Iterator& Ranges::Iterator::operator++()
+{
+    _num++;
+    return *this;
+}
+
+bool Ranges::Iterator::operator==(const Iterator& other) const
+{
+    return _num == other._num;
+}
+
+bool Ranges::Iterator::operator!=(const Iterator& other) const
+{
+    return !(*this == other);
+}
+
+unsigned Ranges::Iterator::operator*() const
+{
+    return _num;
+}
