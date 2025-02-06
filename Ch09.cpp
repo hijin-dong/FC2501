ef03cf76961cf47da9a574a0f5a511d3cb9d5d72
Dongle <96672879+hijin-dong@users.noreply.github.com>
2025-02-06 20:37:25 +0900

    Update Ch09.cpp

--- Ch09.cpp
+++ Ch09.cpp
@@ -802,21 +802,342 @@ int main()
 /*
 	09. std::thread
 */
+//# include <iostream>
+//# include <thread>
+//# include <chrono>
+//using namespace std;
+//
+//// ~2.
+//struct Test
+//{
+//	int num;
+//	Test(int num) : num(num) { cout << "Construct: " << num << endl; }
+//	~Test() { cout << "Destruct: " << num << endl; }
+//};
+//
+//void foo0()
+//{
+//	for (int i = 0; i < 5; i++)
+//	{
+//		// static int num = 10;
+//		thread_local int num = 10;
+//		num++;
+//		cout << num << endl;
+//	}
+//}
+//
+//// ~3.
+//int num0 = 0;
+//void foo1()
+//{
+//	for (int i = 0; i < 1000000; i++)
+//		num0 += 1;
+//}
+//
+//bool flag = false; // volatile 없으면 되었다 안되었다 함
+//// 위의 예시 num0에 volatile 붙인다고 되지는 않음
+//// 그리고 volatile 자체를 어쩔 수 없는 상황이 아닌 이상 사용 X
+//int value = 0;
+//
+//int main()
+//{
+//	// 1) thread 개괄
+//	cout << "========== Example 1 ==========" << endl;
+//	thread th0; // 운영체제의 할당을 받지 않은 빈 스레드 객체
+//
+//	cout << "thread ID: " << this_thread::get_id() << endl;
+//	thread th1([]
+//		{
+//			cout << "thread ID: " << this_thread::get_id() << endl;
+//			this_thread::sleep_for(1s);
+//		}); // callable 한 객체를 넣어 할당 요청
+//	// this_thread::sleep_for(2s); // join 대신 th1이 끝날때까지 기다림
+//	cout << boolalpha;
+//	cout << th1.joinable() << endl; // join 가능 여부 확인
+//	th1.join(); // thread가 끝날때까지 대기. join 하지 않으면 문제 발생
+//	// (위의 sleep 활성화하면 시간차로 완료되기 때문에 ok!)
+//	cout << "Complete" << endl;
+//	// th1.detach(); // thread 분리. th1을 기다리지 않음
+//	thread th2 = move(th1); // 복사 불가. move만 가능
+//
+//	// 2) local thread 공간
+//	cout << "========== Example 2 ==========" << endl;
+//	// foo 함수 내부에서 static을 활성화하면 11~20 출력
+//	// thread_local을 활성화하면 11~15 2회 출력
+//	thread(foo0).join();
+//	foo0();
+//
+//	// 3) thread 동기화의 필요성
+//	cout << "========== Example 3 ==========" << endl;
+//		// 3.1) 예시 1
+//	thread th3(foo1);
+//	foo1();
+//	th3.join();
+//	cout << "3.1: " << num0 << endl; // 1236313. 200만이 나와야하는데 그러지 않음! (race condition)
+//	// num += 1 연산은 아래와 같이 세 개로 쪼개져 있음
+//		// 1. load num
+//		// 2. register += 1
+//		// 3. store register
+//		// thread0  |  thread1
+//		// 1 reg(0)
+//		//             1 reg(0)
+//		// 2 reg(1)    2 reg(1)
+//	  // 3 num(1)    3 num(1)
+//	// 지금 상황은 thread 2개(Core 2개) 여서 발생한 문제이나, 단일 코어에서도 동일 문제 발생 가능
+//		// 물리적으로 thread가 동시에 한 개만 작동할 수 있더라도 스위칭을 통해 thread0 실행 중에 thread1로 갔다올 수 있음
+//		// 혹은 컴파일 과정에서 사람이 코딩한 것과 다른 순서로 동작하게 되기도 함
+//			// (ex. int num = 0; cin << num; 순서로 작성하면 cin 받은 후에 num =0 할당)
+//
+//		// 3.2) 예시 2
+//	thread th4([]
+//		{
+//			value = 100;
+//			flag = true;
+//		});
+//	thread th5([]
+//		{
+//			while (!flag);
+//			cout << value << endl;
+//		});
+//}
+
+/*
+	10. std::thread 동기화 문제 해결하기
+*/
+//# include <iostream>
+//# include <thread>
+//# include <mutex>
+//# include <atomic>
+//# include <queue>
+//using namespace std;
+//
+//// ~1.
+//void foo(int& num)
+//{
+//	num++;
+//	throw runtime_error("err");
+//}
+//
+//recursive_mutex m1;
+//void hoo()
+//{
+//	lock_guard<recursive_mutex> lock(m1);
+//}
+//
+//void goo()
+//{
+//	lock_guard<recursive_mutex> lock(m1);
+//	hoo();
+//}
+//
+//// ~3.
+//void produce(mutex& m, condition_variable& cv, queue<int>& jobQueue)
+//{
+//	while (true)
+//	{
+//		this_thread::sleep_for(100ms); // 100ms마다
+//		{
+//			lock_guard<mutex> lock(m);
+//			jobQueue.push(1);
+//			cout << "Produce: " << jobQueue.size() << endl;
+//		}
+//		cv.notify_one();
+//		// cv.notify_all(); // 기다리고 있는 모두에게 noti
+//	}
+//}
+//
+//void longTimeJob()
+//{
+//	this_thread::sleep_for(200ms);
+//}
+//
+//void consume(mutex& m, condition_variable& cv, queue<int>& jobQueue)
+//{
+//	while (true)
+//	{
+//		{
+//			unique_lock<mutex> lock(m); // cv.wait 사용하려면 unique_lock
+//			if (jobQueue.empty()) // lost wakeup: wait 전에 notify를 보냈을 수 있음 -> wait 전에 noti 확인
+//				cv.wait(lock);
+//
+//			if (jobQueue.empty()) // spurious wakeup: 실제로 notify를 하지 않았는데도 깨어날 수 있음
+//				continue;
+//
+//			int result = jobQueue.front();
+//			jobQueue.pop();
+//
+//			cout << "Consume: " << jobQueue.size() << endl;
+//		}
+//		longTimeJob();
+//	}
+//}
+//
+//int main()
+//{
+	// 1) critical section 설정: 한 스레드만 접근 가능
+	//cout << "========== Example 1 ==========" << endl;
+	//mutex m; // mutual exclusive: 상호 배제
+	//int num = 0;
+	//auto func = [&num, &m]
+	//	{
+	//		for (int i = 0; i < 1000000; i++)
+	//		{
+	//			//// m.lock(); // unlock되기 전까지 critical section
+	//			//// exception이 나는 경우에 더 진행이 안되는 문제 발생 -> RAII
+	//			//lock_guard<mutex> lock(m);
+	//			//num++;
+	//			//m.unlock();
+	//			// -> 더 간소화해서 적기
+	//			try
+	//			{
+	//				// 중괄호 영역이 critical section이 되고, exception 발생 시 destructor 실행되며 unlock
+	//				lock_guard<mutex> lock(m);
+	//				foo(num);
+	//				num++;
+	//			}
+	//			catch (...)
+	//			{}
+	//		}
+	//	};
+	//thread th6(func);
+	//thread th7(func);
+	//th6.join();
+	//th7.join();
+	//cout << num << endl; // 2000000
+
+		// 1.1) 다른 lock 방식들
+		// m.try_lock(); - lock 걸기 시도, 걸지 못하면 false 리턴
+		// unique_lock<mutex> lock(m, try_to_lock); - 기존 lock_guard와 달리 try_lock 옵션 추가 가능
+			// if (locks.owns_lock()) { /* critical session */ }
+
+		// 1.2) 다양한 mutex
+		// timed_mutex
+			// m.try_lock_for(10s); - 10초 안에 lock이 걸리면 성공, 실패서 false 리턴
+		// recursive_mutex
+			// 하나라도 걸리면 다른 스레드는 접근 불가, 예시에서는 hoo와 goo가 같은 스레드여서 recursive_lock 가능
+			// 이런 경우에는 lock을 건 횟수만큼 unlock 필요 (개수 부족 시에 lock 안 풀림)
+	// hoo();
+
+	// 2) atomic
+	// 특정 "구역"이 아닌 변수에 lock을 걸때는 critical section 대신 atomic 키워드 사용이 성능 효율
+//	cout << "========== Example 2 ==========" << endl;
+//	atomic_int num1 = 0;
+//	auto func1 = [&num1]
+//		{
+//			for (int i = 0; i < 1000000; i++)
+//				num1++;
+//		};
+//
+//	thread th8(func1);
+//	thread th9(func1);
+//	th8.join();
+//	th9.join();
+//	cout << num1 << endl; // 2000000
+//
+//	// 3) 조건 변수
+//	// 특정 조건이 될 때까지 스레드가 대기 & 여러 스레드가 조건변수 공유 & 조건 만족 시 notify
+//	cout << "========== Example 3 ==========" << endl;
+//	mutex m2;
+//	condition_variable cv;
+//	queue<int> jobQueue;
+//
+//	thread producer(produce, ref(m2), ref(cv), ref(jobQueue));
+//	thread consumer(consume, ref(m2), ref(cv), ref(jobQueue));
+//
+//	producer.join();
+//	consumer.join();
+//}
+
+/*
+	11. std::future, std::promise, std::packaged_task, std::async
+	- future: 수신자, 미래에 어떤 값을 수신하겠다!
+	- promise: 송신자: 미래에 어떤 값을 송신하겠다!
+*/
 # include <iostream>
-# include <thread>
+# include <future>
 # include <chrono>
 using namespace std;
 
 int main()
 {
-	cout << "thread ID: " << this_thread::get_id() << endl;
+	// 1) promise -> future + 스레드 사용
+	cout << "========== Example 1 ==========" << endl;
+	promise<int> pro0;
+	future<int> fut0 = pro0.get_future();
 
-	thread th0; // 운영체제의 할당을 받지 않은 빈 스레드 객체
-	thread th1([]
+	thread th0([](promise<int> pro)
 		{
-			cout << "thread ID: " << this_thread::get_id() << endl;
-			this_thread::sleep_for(1s);
-		}); // callable 한 객체를 넣어 할당 요청
-	th1.join(); // thread가 끝날때까지 대기
-	cout << "Complete" << endl;
+			try
+			{
+				this_thread::sleep_for(2s);
+				pro.set_value(10); // 이 줄을 주석처리할 경우 blocking이 되어 계속 걸려있게 됨 & 한 번만 세팅 가능
+			}
+			catch (...)
+			{
+				exception_ptr e = current_exception();
+				pro.set_exception(e); // exception을 넘겨주는 것도 가능!
+			}
+		}, move(pro0));
+
+	try
+	{
+		cout << fut0.get() << endl; // 10. get을 할 경우 이동하기 때문에 한 번만 가능
+		// 실행시 발생하는 딜레이는 2s 대기 후 get이 이루어지기 때문
+	}
+	catch (...)
+	{
+		cout << "err" << endl;
+	}
+	th0.join();
+
+	// 2) Packaged_task + 스레드 사용
+	// 앞서 사용한 promise와 try-catch문 간소화 가능
+	cout << "========== Example 2 ==========" << endl;
+	packaged_task<int(int)> task([](int value) // packaged_task를 통한 1번의 추상화
+		{
+			if (value < 0)
+				// packaged_task에 try-catch도 포함되어 있기에 따로 세팅 필요 X
+				throw runtime_error("err");
+
+			// 1번에서는 여기서 promise에 값을 세팅해주어야 했음
+			return value;
+		});
+
+	future<int> fut1 = task.get_future();
+	// thread th1(move(task), 10);
+	thread th1(move(task), -1);
+	try
+	{
+		cout << fut1.get() << endl; // 10, err
+	}
+	catch (const runtime_error& err)
+	{
+		cout << err.what() << endl;
+	}
+	th1.join(); // 혹시 이것까지 추상화가 가능할까..?
+
+	// 3) async
+	// async는 내부적으로 thread를 생성함
+	cout << "========== Example 3 ==========" << endl;
+	cout << "thread id: " << this_thread::get_id() << endl;
+	// 옵션이 없으면 async가 스레드 생성 여부 판단 (launch::deferred | launch::async)
+	// launch::deferred, 을 추가하여 스레드 생성 억제 및 기존 스레드 사용하도록 할 수 있음
+		// 하단 fut2.get()를 만날 때까지 내부를 실행하지 않음 (실행을 연기함!)
+	// launch::async, 을 추가하여 반드시 다른 스레드를 사용하도록 할 수 있음
+	future fut2 = async(launch::deferred, [](int value)
+		{
+			cout << "thread id: " << this_thread::get_id() << endl;
+			if (value < 0)
+				throw runtime_error("err");
+			return value;
+		}, 100);
+
+	try
+	{
+		cout << fut2.get() << endl; // 100
+	}
+	catch (const runtime_error& err)
+	{
+		cout << err.what() << endl; // err
+	}
 }
